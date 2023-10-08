3b2923c6d2ddb0d4f8fc4476cf3054ad871ccd4b
Natalja-Markova <nataljamarkova71@gmail.com>
2023-10-08 22:24:05 +0200

    34: Projekt was created, add pom.xml and Class for test

--- src/test/java/de/ait/homework34/StringProcessorTest.java
+++ src/test/java/de/ait/homework34/StringProcessorTest.java
@@ -1,5 +1,38 @@
 package de.ait.homework34;
 
-public class StringProcessorTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class StringProcessorTest {
+    private StringProcessor stringProcessor;
+
+    @BeforeEach
+    public void setUp() {
+        stringProcessor = new StringProcessor();
+    }
+
+    @Test
+    public void testConcatenate() {
+       assertEquals ("Hi Nata",
+              stringProcessor.concatenate("Hi"," Nata") );
+
+    }
+
+    @Test
+    public void  testgetLenght() {
+        assertEquals(7, stringProcessor.getLength("Hi Nata"));
+    }
+
+    @Test
+    public void  testrevers() {
+        assertEquals("asor",
+                stringProcessor.reverse("rosa"));
+    }
+
+    @Test
+    public void testisPalindrome() {
+        assertEquals(
+                true, stringProcessor.isPalindrome("madam"));
+    }
 }
